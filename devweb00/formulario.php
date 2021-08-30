1b7600aa5dd7890197bd026650336009e4c02b6c
leonardorici <88636808+leonardorici@users.noreply.github.com>
2021-08-30 17:32:09 -0300

    Add files via upload

---
+++ devweb00/formulario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados</title>
+</head>
+<body>
+    <h1> Dados preenchidos: </h1>
+<?php 
+    echo print_r ($_POST, true);
+    echo "<br>";
+?> 
+ 
+</body>
+</html>
---
+++ devweb00/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desenvolvimento Web</title>
+</head>
+<style>
+
+    .container{
+        display: inline-block;
+        padding: 5px;
+        border-style: solid;
+        border-color: black;
+        border-width: 0.5px;
+        border-radius: 5px;
+        background-color:rgba(79, 79, 80, 0.548); 
+    }
+
+    a{
+        color: white;
+        font-family: Gill Sans, sans-serif;
+        font-size: 9px;
+        font-style: italic;
+    }
+
+    .caixona{
+            display: inline-block;
+            padding: 15px;
+            padding-bottom: 20px;
+            border-style: solid;
+            border-color: black;
+            border-width: none;
+            border-radius: 5px;
+            background-image:url("fundo.jpg");
+            
+        }
+
+        
+    
+</style>
+<body>
+    <div class=caixona>
+        <div class=container>
+            <a href="form.html"> Fazer loguin em nosso site </a>
+        </div>
+    </div>   
+</body>
+</html>
